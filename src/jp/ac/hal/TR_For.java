def89f1df349c0e980f1daca48c45cde7fdc16c5
ryoki726 <ryooki.0726@gmail.com>
2019-08-07 21:32:08 +0900

    i が 3 で割り切れる時だけ出力する

--- src/jp/ac/hal/TR_For.java
+++ src/jp/ac/hal/TR_For.java
@@ -5,7 +5,10 @@ public class TR_For {
 	public static void main(String[] args) {
 		int sum = 0;
 		for (int i = 1; i < 11; i++) {
-
+			if (i % 2 == 0){
+                System.out.println(i);
+                sum += i;
+            }
         }
 	}
 
