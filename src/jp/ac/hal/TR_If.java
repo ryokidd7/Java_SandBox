b9bfc9d27990fb09d084dd23df8797d52e27545f
ryoki726 <ryooki.0726@gmail.com>
2019-08-08 14:57:44 +0900

    点数を用いてIF文でランクを条件分岐

--- src/jp/ac/hal/TR_If.java
+++ src/jp/ac/hal/TR_If.java
@@ -4,6 +4,14 @@ public class TR_If {
 	public static void main(String[] args) {
 		int score = 80;
         char rank;
-
+        if (score >= 95) {
+            rank='A';
+        } else if (score >= 75) {
+            rank='B';
+        } else if (score >= 50) {
+            rank='C';
+        } else {
+            rank='D';
+        }
     }
 }
