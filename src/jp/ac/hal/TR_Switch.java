55beb8a219d29fb0aac30c8e2185920fa1d9a69c
ryoki726 <ryooki.0726@gmail.com>
2019-08-08 15:31:02 +0900

    該当するcaseを探す

--- src/jp/ac/hal/TR_Switch.java
+++ src/jp/ac/hal/TR_Switch.java
@@ -4,7 +4,11 @@ public class TR_Switch {
 	public static void main(String[] args) {
         int food = 10;
         switch (food) {
-
-    }
+	        case 1: System.out.println("ハンバーグ"); break;
+	        case 2: System.out.println("オムライス"); break;
+	        case 3: System.out.println("寿司"); break;
+	        case 4: System.out.println("パスタ"); break;
+	        default: System.out.println("小籠包"); break;
+	    }
     }
 }
