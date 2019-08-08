7ba3fd1669b39e52bf1cd1fdf50808344a13a611
ryoki726 <ryooki.0726@gmail.com>
2019-08-08 15:21:07 +0900

    ？：演算子を使いランク付けする

--- src/jp/ac/hal/TR_QuestionColon.java
+++ src/jp/ac/hal/TR_QuestionColon.java
@@ -3,5 +3,7 @@ package jp.ac.hal;
 public class TR_QuestionColon {
 	public static void main(String[] args) {
 		int score = 75;
+		System.out.println("あなたは" + (score >= 80 ? "合格" : "不合格") + "です。");
+		//式 ？ 文１ ： 文2；
     }
 }
