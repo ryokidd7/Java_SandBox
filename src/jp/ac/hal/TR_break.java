5c7e969a389f4478e7a1aba9b2c0dba68c562267
ryoki726 <ryooki.0726@gmail.com>
2019-08-09 21:23:46 +0900

    配列lotteryに値を代入する:当たり値を設定

--- src/jp/ac/hal/TR_break.java
+++ src/jp/ac/hal/TR_break.java
@@ -1,5 +1,9 @@
 package jp.ac.hal;
 
 public class TR_break {
-
+	public static void main(String[] args) {
+		int[][] lottery = {{ 4, 35, 63, 16, 50 },{ 59, 89, 12, 32, 9 }};
+	    int award = 50;
+	    boolean judge = false;
+    }
 }
