467899b32ed2e5a3f59c47994b8471240dcb12e7
ryoki726 <ryooki.0726@gmail.com>
2019-08-10 20:01:12 +0900

    命名規則統一

--- src/jp/ac/hal/TR_break.java
+++ src/jp/ac/hal/TR_break.java
@@ -1,6 +1,6 @@
 package jp.ac.hal;
 
-public class TR_break {
+public class TR_Break {
 	public static void main(String[] args) {
 		int[][] lottery = {{ 4, 35, 63, 16, 50 },{ 59, 89, 12, 32, 9 }};
 	    int award = 50;
