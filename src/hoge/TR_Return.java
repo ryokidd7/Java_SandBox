b0ca8ba3e6701834a331809d2055b1f213cfd3bc
ryoki726 <ryooki.0726@gmail.com>
2019-08-12 20:27:40 +0900

    taxCal内で、変数pと0.05を乗算し、結果をtaxPriceに代入:return文でtaxCalを終了

--- src/hoge/TR_Return.java
+++ src/hoge/TR_Return.java
@@ -1,6 +1,10 @@
 package hoge;
 
 public class TR_Return {
+	static float taxCal(int p) {
+        float taxPrice = 0.05F * p;
+        return taxPrice;
+    }
 	public static void main(String[] args) {
         int price = 100;
         int tax = (int)taxCal(price);
