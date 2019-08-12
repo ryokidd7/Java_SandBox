7a6f5dcd9fba0125a1965d78be67bac17d6346ab
ryoki726 <ryooki.0726@gmail.com>
2019-08-12 20:28:41 +0900

    priceとtaxを加算し、totalに値の代入:totalの表示

--- src/hoge/TR_Return.java
+++ src/hoge/TR_Return.java
@@ -8,5 +8,7 @@ public class TR_Return {
 	public static void main(String[] args) {
         int price = 100;
         int tax = (int)taxCal(price);
+        int total = price + tax;
+        System.out.println("合計は" + total + "円です。");
     }
 }
