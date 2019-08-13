230b78915310cbe2931d782339f5fc1a450cc6e5
ryoki726 <ryooki.0726@gmail.com>
2019-08-13 16:14:53 +0900

    例外の場合にコメントを出す処理

--- src/hoge/TR_Unexpected.java
+++ src/hoge/TR_Unexpected.java
@@ -1,5 +1,12 @@
 package hoge;
 
 public class TR_Unexpected {
-
+	public static void main (String[] args) {
+        try {
+            int x = 10/0;
+        //例外
+        } catch (ArithmeticException e) {
+            System.out.println("0では割れません");
+        }
+    }
 }
