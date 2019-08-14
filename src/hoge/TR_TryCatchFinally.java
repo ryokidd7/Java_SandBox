4af7cd27f56f0488aed29edd0c66befdef222afb
ryoki726 <ryooki.0726@gmail.com>
2019-08-14 22:30:58 +0900

    例外をスローする可能性のある処理をtryの中に

--- src/hoge/TR_TryCatchFinally.java
+++ src/hoge/TR_TryCatchFinally.java
@@ -1,5 +1,10 @@
 package hoge;
 
 public class TR_TryCatchFinally {
-
+	public static void main (String[] args) {
+        try {  //(1)try節
+            int x = Integer.parseInt(args[0]);
+            System.out.println("答えは" + (100/x));
+        }
+    }
 }
