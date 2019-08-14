5022053d89bb5c5d8a01370c8b3a37f9accb2247
ryoki726 <ryooki.0726@gmail.com>
2019-08-14 22:31:46 +0900

    例外処理を記載

--- src/hoge/TR_TryCatchFinally.java
+++ src/hoge/TR_TryCatchFinally.java
@@ -5,6 +5,8 @@ public class TR_TryCatchFinally {
         try {  //(1)try節
             int x = Integer.parseInt(args[0]);
             System.out.println("答えは" + (100/x));
+        } catch(ArithmeticException e) {  //(2)catch節
+            System.err.println("エラー" + e.getMessage());
         }
     }
 }
