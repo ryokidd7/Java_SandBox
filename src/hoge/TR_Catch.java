792b6075871f73982f062f7f9cf5cc678bdf83db
ryoki726 <ryooki.0726@gmail.com>
2019-08-15 10:05:36 +0900

    例外が発生するしないにかかわらずfinally節を実行

--- src/hoge/TR_Catch.java
+++ src/hoge/TR_Catch.java
@@ -6,6 +6,13 @@ public class TR_Catch {
             int x = Integer.parseInt(args[0]);
             System.out.println("答えは" + (100/x));
         //ArithmeticExceptionの例外ハンドラ
+        }catch(ArithmeticException e) {
+            System.err.println("ArithmeticExceptionをキャッチ");
+        //NumberFormatExceptionの例外ハンドラ
+        } catch(NumberFormatException e) {
+            System.err.println("NumberFormatExceptionをキャッチ");
+        } finally {
+            System.out.println("This program was finished");
         }
     }
 }
