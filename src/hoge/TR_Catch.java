ce590f82dad70aefb2c51bfd593ab8c737f2aa86
ryoki726 <ryooki.0726@gmail.com>
2019-08-15 10:03:58 +0900

    入力された値をintに変換xに代入:xで除算

--- src/hoge/TR_Catch.java
+++ src/hoge/TR_Catch.java
@@ -1,5 +1,11 @@
 package hoge;
 
 public class TR_Catch {
-
+	public static void main (String[] args) {
+        try {
+            int x = Integer.parseInt(args[0]);
+            System.out.println("答えは" + (100/x));
+        //ArithmeticExceptionの例外ハンドラ
+        }
+    }
 }
