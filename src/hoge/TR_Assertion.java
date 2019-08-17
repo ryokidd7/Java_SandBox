353c7b7f8b40fbe68ce3ed1428702172c8ba41eb
ryoki726 <ryooki.0726@gmail.com>
2019-08-17 17:46:09 +0900

    数値を設定

--- src/hoge/TR_Assertion.java
+++ src/hoge/TR_Assertion.java
@@ -1,6 +1,10 @@
 package hoge;
 
 public class TR_Assertion {
+	public static void main(String[] args) {
+        TR_Assertion object1 = new TR_Assertion();
+        object1.calNum(5, 10);
+    }
 	void calNum(int x, int y) {
         int z = 0;
         assert x > 0 && y > 0:"xまたはyが0以下です。";
