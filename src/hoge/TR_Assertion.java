3439f44ddafa920f82f7036e02c3bd56e1f7626d
ryoki726 <ryooki.0726@gmail.com>
2019-08-17 17:43:41 +0900

    forループで、xをy回加算。結果はzに代入

--- src/hoge/TR_Assertion.java
+++ src/hoge/TR_Assertion.java
@@ -4,5 +4,10 @@ public class TR_Assertion {
 	void calNum(int x, int y) {
         int z = 0;
         assert x > 0 && y > 0:"xまたはyが0以下です。";
+
+        for (int i = 0; i < y; i++) {
+            z = z + x;
+        }
+
     }
 }
