e723cdc8428b9ab0db1d98422a8aee8c11369a2f
ryoki726 <ryooki.0726@gmail.com>
2019-08-21 19:29:40 +0900

    フォーマットパターンを指定して、SimpleDateFormatオブジェクトsdf1を生成

--- src/hoge/TR_SimpleDateFormat.java
+++ src/hoge/TR_SimpleDateFormat.java
@@ -6,6 +6,8 @@ package hoge;
 public class TR_SimpleDateFormat {
 	public static void main(String[] args) {
 	    Date date1 = new Date();
+
+	    SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy'年'MM'月'dd'日'");
 	  }
 
 }
