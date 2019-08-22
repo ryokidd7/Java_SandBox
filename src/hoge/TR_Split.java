78c0bee8ea86c768db0603c72cc74edc157af06e
ryoki726 <ryooki.0726@gmail.com>
2019-08-22 22:10:36 +0900

    分割された文字列を表示

--- src/hoge/TR_Split.java
+++ src/hoge/TR_Split.java
@@ -4,5 +4,8 @@ public class TR_Split {
 	public static void main(String[] args) {
 	    String str = new String("java,Java,JAVA");
 	    String[] strAry = str.split(",");
+	    for (int i=0; i < strAry.length; i++) {
+	        System.out.println(strAry[i]);
+	      }
 	  }
 }
