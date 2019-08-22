b075c0e2beef7045bf1bccbfa50824b38f07e2d0
ryoki726 <ryooki.0726@gmail.com>
2019-08-22 22:17:24 +0900

    maxメソッド,minメソッドを使い、比較

--- src/hoge/TR_Comparison.java
+++ src/hoge/TR_Comparison.java
@@ -4,5 +4,9 @@ public class TR_Comparison {
 	public static void main(String[] args) {
         double exValue1 = 23.52;
         double exValue2 = 53.38;
+        //20.43と50.12のうち、大きな値を返す
+        System.out.println(Math.max(exValue1, exValue2));
+        //20.43と50.12のうち、小さな値を返す。
+        System.out.println(Math.min(exValue1, exValue2));
     }
 }
