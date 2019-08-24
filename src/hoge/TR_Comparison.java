0389895788c84b8d9bcdc47a310103ee53b5f7b9
ryoki726 <ryooki.0726@gmail.com>
2019-08-24 23:49:53 +0900

    実行結果をコメント

--- src/hoge/TR_Comparison.java
+++ src/hoge/TR_Comparison.java
@@ -8,5 +8,9 @@ public class TR_Comparison {
         System.out.println(Math.max(exValue1, exValue2));
         //20.43と50.12のうち、小さな値を返す。
         System.out.println(Math.min(exValue1, exValue2));
+        //実行結果
+        //53.38
+        //23.52
+
     }
 }
