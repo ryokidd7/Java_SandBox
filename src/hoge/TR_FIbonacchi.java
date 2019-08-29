9c36d85d20d80149ca68b4a0a780e59162ecb814
ryoki726 <ryooki.0726@gmail.com>
2019-08-29 21:07:01 +0900

    0と1はそのまま

--- src/hoge/TR_FIbonacchi.java
+++ src/hoge/TR_FIbonacchi.java
@@ -3,5 +3,10 @@ package hoge;
 public class TR_FIbonacchi {
 	public static void main(String[] args) {
 		int f0, f1, fn;
+		f0 = 0;
+		System.out.println( f0 );
+
+		f1 = 1;
+		System.out.println( f1 );
 	}
 }
